0e15c62f5ff8bdce6954db019b73a3d33c03180b
Marc Lainez <marc.lainez@gmail.com>
2025-04-19 07:34:16 +0200

    Use latest forks of qmi and vintage_net_qmi for fairphone2 modem to work

--- config/nerves_system_fairphone2.exs
+++ config/nerves_system_fairphone2.exs
@@ -6,5 +6,18 @@ config :vintage_net,
   config: [
     {"usb0", %{type: VintageNetDirect}},
     {"eth0", %{type: VintageNetEthernet, ipv4: %{method: :dhcp}}},
-    {"wlan0", %{type: VintageNetWiFi}}
+    {"wlan0", %{type: VintageNetWiFi}},
+    {"wwan0",
+      %{
+        type: VintageNetQMI,
+        vintage_net_qmi: %{
+          service_providers: [%{apn: "simbase"}],
+          device_path: "/dev/wwan0qmi0"
+        }
+      }
+    }
   ]
+
+config :logger,
+  backends: [RingLogger, :console],
+  level: :debug
